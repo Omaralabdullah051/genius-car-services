41cff459e4b32ea5ebea94bf7055b6d6aad664fc
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-04-19 14:33:38 +0400

    implemented react helmet to display dynamic title

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { useAuthState, useSendEmailVerification } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
+import { ToastContainer, toast } from 'react-toastify';
 
 const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
+    const [sendEmailVerification, sending, error] = useSendEmailVerification(auth);
     const location = useLocation();
 
     if (loading) {
@@ -13,6 +15,24 @@ const RequireAuth = ({ children }) => {
     }
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />
+    }
+
+    if (!user.emailVerified) {
+        return (
+            <div>
+                <h3 className='text-danger'>Your Email is not verified!</h3>
+                <h5 className='text-success'>Please Verify your email address</h5>
+                <button className='btn btn-primary'
+                    onClick={async () => {
+                        await sendEmailVerification();
+                        toast('Sent email');
+                    }}
+                >
+                    Send Verification email again
+                </button>
+                <ToastContainer />
+            </div>
+        )
     }
     return children;
 };
